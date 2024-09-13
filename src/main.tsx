6402e00b6eab93cc9ce38c92b05b41bb4178901a
ajin <aj5210@naver.com>
2024-09-13 18:39:18 +0900

    Chore: Chakra UI 라이브러리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
 )
