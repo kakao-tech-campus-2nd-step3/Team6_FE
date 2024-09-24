46bdb447fab7d5f3b82a02318467d5ab16deb519
Solbin Jung <jsolbin0724@gmail.com>
2024-09-25 00:53:34 +0900

    Feat: 질문 텍스트 추가

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,19 @@
-export default function MainPage() {
-  return <div>메인 페이지</div>
+import { Box, Text } from '@chakra-ui/react'
+
+const MainPage = () => {
+  return (
+    <Box
+      bg="secondary_background"
+      p={20}
+      borderRadius="20px"
+      textAlign="center"
+    >
+      {/* font.ts가 머지되면 600으로 사이즈 변경하기 */}
+      <Text fontWeight="bold" fontSize="4xl" color="text" mb={12}>
+        가장 MZ스러운 사람
+      </Text>
+    </Box>
+  )
 }
+
+export default MainPage
