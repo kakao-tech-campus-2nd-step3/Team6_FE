b1964502ac6468d85fc316e0f7235cde6793577c
ajin <aj5210@naver.com>
2024-10-01 18:38:36 +0900

    Desgin: 사이드 네비게이션 아이콘 UI 추가

--- src/components/common/PageLayout/SideNavigation/index.tsx
+++ src/components/common/PageLayout/SideNavigation/index.tsx
@@ -1,5 +1,29 @@
-import { Box } from '@chakra-ui/react'
+import { BiBell, BiUserCircle } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+
+import { Box, Flex } from '@chakra-ui/react'
 
 export const SideNavigation = () => {
-  return <Box background="brown.300" width="40px" height="full" />
+  return (
+    <Flex
+      background="brown.300"
+      width="40px"
+      height="full"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="end"
+      paddingY={6}
+      color="brown.600"
+      gap={2}
+    >
+      <Box _hover={{ cursor: 'pointer', color: 'brown.500' }}>
+        <BiBell size={26} />
+      </Box>
+      <Link to="/mypage">
+        <Box _hover={{ cursor: 'pointer', color: 'brown.500' }}>
+          <BiUserCircle size={26} />
+        </Box>
+      </Link>
+    </Flex>
+  )
 }
