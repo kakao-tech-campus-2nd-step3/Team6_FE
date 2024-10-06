985c3255ff1967828d8dc6d5ac54d746a3aac430
Solbin Jung <jsolbin0724@gmail.com>
2024-10-06 14:05:10 +0900

    Feat: 타입 단언 제거

--- src/pages/GroupPage/Management/index.tsx
+++ src/pages/GroupPage/Management/index.tsx
@@ -1,37 +1,36 @@
-import { IconType } from 'react-icons'
 import { BiCog, BiGroup, BiLink, BiPlus } from 'react-icons/bi'
 
 import { Box, Flex, Text } from '@chakra-ui/react'
 
-import CardButton from '@/components/common/CardButton'
+import CardButton, { CardButtonProps } from '@/components/common/CardButton'
 
-const CardData1 = {
-  variant: 'orange' as const,
-  orientation: 'vertical' as const,
+const CardData1: CardButtonProps = {
+  variant: 'orange',
+  orientation: 'vertical',
   label: '질문 관리',
   description: '그룹 질문을 관리해보세요',
-  Icon: BiCog as IconType,
+  Icon: BiCog,
 }
-const CardData2 = {
-  variant: 'white' as const,
-  orientation: 'vertical' as const,
+const CardData2: CardButtonProps = {
+  variant: 'white',
+  orientation: 'vertical',
   label: '멤버 관리',
   description: '그룹 멤버를 관리해보세요',
-  Icon: BiGroup as IconType,
+  Icon: BiGroup,
 }
-const CardData3 = {
-  variant: 'white' as const,
-  orientation: 'horizontal' as const,
+const CardData3: CardButtonProps = {
+  variant: 'white',
+  orientation: 'horizontal',
   label: '초대하기',
   description: '새로운 멤버를 초대해보세요',
-  Icon: BiLink as IconType,
+  Icon: BiLink,
 }
-const CardData4 = {
-  variant: 'white' as const,
-  orientation: 'horizontal' as const,
+const CardData4: CardButtonProps = {
+  variant: 'white',
+  orientation: 'horizontal',
   label: '질문 추가',
   description: '그룹 질문을 건의해보세요',
-  Icon: BiPlus as IconType,
+  Icon: BiPlus,
 }
 
 interface ManagementProps {
