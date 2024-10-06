812738b2d2c7fea52fafa96efd03128ff793cde7
anheejeong <eyrt6973@naver.com>
2024-10-06 21:57:54 +0900

    Feat: 프로필 질문 채팅 답변 ui 구현

---
+++ src/pages/ProfileQuestionPage/WriteReply/index.tsx
@@ -0,0 +1,30 @@
+// components/WriteReply.tsx
+import { Button, Flex, Textarea } from '@chakra-ui/react'
+
+export default function WriteReply() {
+  return (
+    <Flex
+      height="170px"
+      bg="white"
+      padding="0 5px"
+      flexDirection="column"
+      justifyContent="space-evenly"
+    >
+      <Textarea width="100%" height="auto" variant="unstyled" padding="0" />
+      <Flex justifyContent="end" height="29px" margin="4px 0">
+        <Button
+          marginRight="15px"
+          borderRadius={20}
+          width="90px"
+          height="30px"
+          bg="brown.400"
+          fontSize="small"
+          _hover={{ boxShadow: 'md' }}
+          _active={{ bg: 'brown.500' }}
+        >
+          전송
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
