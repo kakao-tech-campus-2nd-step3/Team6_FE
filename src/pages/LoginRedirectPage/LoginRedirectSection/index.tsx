c706c1b47420a013c772256d48bd859d25497f88
ajin <aj5210@naver.com>
2024-10-07 21:21:37 +0900

    Fix: navigate 함수 useEffect 내부에서 실행하도록 수정

--- src/pages/LoginRedirectPage/LoginRedirectSection/index.tsx
+++ src/pages/LoginRedirectPage/LoginRedirectSection/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import { useKakaoLogin } from '@/api/services/login'
@@ -13,14 +14,16 @@ export const LoginRedirectSection = ({ code }: LoginRedirectSectionProps) => {
   const { data, status, error } = useKakaoLogin({ code })
   const setAuthToken = useAuthTokenStore((state) => state.setAuthToken)
 
+  useEffect(() => {
+    if (data) {
+      setAuthToken(data.accessToken)
+      navigate('/')
+    }
+  }, [data, setAuthToken, navigate])
+
   if (status === 'pending') return <div>loading...</div>
 
   if (error) return <div>{error.message}</div>
-
-  if (data) {
-    setAuthToken(data.accessToken)
-    navigate('/')
-  }
 
   return <div>loading...</div>
 }
