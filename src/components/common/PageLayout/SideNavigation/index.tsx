13896946acc809dfa913bcc2c27c240cac1f2b38
ajin <aj5210@naver.com>
2024-10-07 11:49:19 +0900

    Feat: 테스트용 로그아웃 추가

--- src/components/common/PageLayout/SideNavigation/index.tsx
+++ src/components/common/PageLayout/SideNavigation/index.tsx
@@ -1,9 +1,13 @@
-import { BiBell, BiUserCircle } from 'react-icons/bi'
+import { BiBell, BiLogOut, BiUserCircle } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 
 import { Box, Flex } from '@chakra-ui/react'
 
+import { useAuthTokenStore } from '@/stores/auth-token'
+
 export const SideNavigation = () => {
+  const clearAuthToken = useAuthTokenStore((state) => state.clearAuthToken)
+
   return (
     <Flex
       background="brown.300"
@@ -16,6 +20,13 @@ export const SideNavigation = () => {
       color="brown.600"
       gap={2}
     >
+      <Box
+        color="brown.400"
+        _hover={{ cursor: 'pointer', color: 'brown.500' }}
+        onClick={() => clearAuthToken()}
+      >
+        <BiLogOut size={26} />
+      </Box>
       <Box _hover={{ cursor: 'pointer', color: 'brown.500' }}>
         <BiBell size={26} />
       </Box>
