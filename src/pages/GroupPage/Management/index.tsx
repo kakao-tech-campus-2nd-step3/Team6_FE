a2c24bed22b920644de05abe28d734f033393574
Solbin Jung <jsolbin0724@gmail.com>
2024-10-10 16:42:20 +0900

    Feat: 더미데이터 배열로 정의

--- src/pages/GroupPage/Management/index.tsx
+++ src/pages/GroupPage/Management/index.tsx
@@ -4,34 +4,36 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 
 import CardButton, { CardButtonProps } from '@/components/common/CardButton'
 
-const CardData1: CardButtonProps = {
-  variant: 'orange',
-  orientation: 'vertical',
-  label: '질문 관리',
-  description: '그룹 질문을 관리해보세요',
-  Icon: BiCog,
-}
-const CardData2: CardButtonProps = {
-  variant: 'white',
-  orientation: 'vertical',
-  label: '멤버 관리',
-  description: '그룹 멤버를 관리해보세요',
-  Icon: BiGroup,
-}
-const CardData3: CardButtonProps = {
-  variant: 'white',
-  orientation: 'horizontal',
-  label: '초대하기',
-  description: '새로운 멤버를 초대해보세요',
-  Icon: BiLink,
-}
-const CardData4: CardButtonProps = {
-  variant: 'white',
-  orientation: 'horizontal',
-  label: '질문 추가',
-  description: '그룹 질문을 건의해보세요',
-  Icon: BiPlus,
-}
+const CardData: CardButtonProps[] = [
+  {
+    variant: 'orange',
+    orientation: 'vertical',
+    label: '질문 관리',
+    description: '그룹 질문을 관리해보세요',
+    Icon: BiCog,
+  },
+  {
+    variant: 'white',
+    orientation: 'vertical',
+    label: '멤버 관리',
+    description: '그룹 멤버를 관리해보세요',
+    Icon: BiGroup,
+  },
+  {
+    variant: 'white',
+    orientation: 'horizontal',
+    label: '초대하기',
+    description: '새로운 멤버를 초대해보세요',
+    Icon: BiLink,
+  },
+  {
+    variant: 'white',
+    orientation: 'horizontal',
+    label: '질문 추가',
+    description: '그룹 질문을 건의해보세요',
+    Icon: BiPlus,
+  },
+]
 
 interface ManagementProps {
   role: 'leader' | 'member'
@@ -41,8 +43,8 @@ export default function Management({ role }: ManagementProps) {
   return (
     <Box p="30px">
       <Flex gap={4} marginTop="10px" marginBottom="16px">
-        <CardButton buttonElement={CardData3} />
-        <CardButton buttonElement={CardData4} />
+        <CardButton buttonElement={CardData[2]} />
+        <CardButton buttonElement={CardData[3]} />
       </Flex>
 
       {role === 'leader' && (
@@ -70,8 +72,8 @@ export default function Management({ role }: ManagementProps) {
             </Text>
           </Box>
           <Flex gap={4}>
-            <CardButton buttonElement={CardData1} />
-            <CardButton buttonElement={CardData2} />
+            <CardButton buttonElement={CardData[0]} />
+            <CardButton buttonElement={CardData[1]} />
           </Flex>
         </Box>
       )}
