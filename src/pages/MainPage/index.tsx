a8d9c3975c4bc95ff16b83c8f7fee165bac773f0
Solbin Jung <jsolbin0724@gmail.com>
2024-10-18 01:19:38 +0900

    Feat: Reload 버튼 기능 구현

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,9 +1,10 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 
 import { Box } from '@chakra-ui/react'
 
 import { useKakaoFriends } from '@/api/services/answer'
 import { Loading } from '@/components/Loading'
+import { Friend } from '@/types'
 
 import ProfileGrid from './ProfileGrid'
 import Question from './Question'
@@ -30,6 +31,9 @@ const Content = ({
   setquestionIndex: React.Dispatch<React.SetStateAction<number>>
 }) => {
   const { data: all = [] } = useKakaoFriends()
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [topProfiles, setTopProfiles] = useState<Friend[]>([])
+  const [bottomProfiles, setBottomProfiles] = useState<Friend[]>([])
 
   const handleProfileSelect = (profileId: number) => {
     console.log(`${profileId}`) // 선택된 프로필 ID 확인하기
@@ -39,12 +43,48 @@ const Content = ({
   const handleSkip = () => {
     setquestionIndex((prevIndex) => (prevIndex + 1) % all.length)
   }
-  // reloadProfiles 테스트
 
-  /* const handleReload = async () => {
-    const data = await refreshProfiles()
-    console.log('프로필 새로 불러오기:', data)
-  } */
+  const handleReload = () => {
+    const totalProfiles = all.length
+    const nextIndex = (currentIndex + 5) % totalProfiles
+    setCurrentIndex(nextIndex)
+  }
+
+  useEffect(() => {
+    const remainingProfiles: Friend[] = all.slice(currentIndex) // 현재 인덱스 이후의 프로필
+    const remainingCount = remainingProfiles.length
+
+    if (remainingCount === 0) {
+      // 남은 데이터가 없으면 처음부터 시작
+      setTopProfiles(all.slice(0, 3))
+      setBottomProfiles(all.slice(3, 5))
+      setCurrentIndex(0) // 다시 0으로 초기화
+    } else if (remainingCount === 1) {
+      setTopProfiles([remainingProfiles[0], all[0], all[1]])
+      setBottomProfiles([all[2], all[3]])
+    } else if (remainingCount === 2) {
+      setTopProfiles([remainingProfiles[0], remainingProfiles[1], all[0]])
+      setBottomProfiles([all[1], all[2]])
+    } else if (remainingCount === 3) {
+      setTopProfiles([
+        remainingProfiles[0],
+        remainingProfiles[1],
+        remainingProfiles[2],
+      ])
+      setBottomProfiles([all[0], all[1]])
+    } else if (remainingCount === 4) {
+      setTopProfiles([
+        remainingProfiles[0],
+        remainingProfiles[1],
+        remainingProfiles[2],
+      ])
+      setBottomProfiles([remainingProfiles[3], all[0]])
+    } else {
+      // 5개 이상의 남은 프로필이 있을 경우
+      setTopProfiles(remainingProfiles.slice(0, 3))
+      setBottomProfiles(remainingProfiles.slice(3, 5))
+    }
+  }, [currentIndex, all])
 
   return (
     <Box
@@ -55,14 +95,14 @@ const Content = ({
     >
       <Question questionIndex={questionIndex} />
       <ProfileGrid
-        profiles={all.slice(0, 3)}
+        profiles={topProfiles}
         onProfileSelect={handleProfileSelect}
       />
       <ProfileGrid
-        profiles={all.slice(3, 5)}
+        profiles={bottomProfiles}
         onProfileSelect={handleProfileSelect}
       />
-      <Buttons onReload={() => {}} onSkip={handleSkip} />
+      <Buttons onReload={handleReload} onSkip={handleSkip} />
     </Box>
   )
 }
