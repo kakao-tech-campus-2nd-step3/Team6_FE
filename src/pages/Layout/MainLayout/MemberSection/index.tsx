b40a1090073db4fd822e4e6e0101d8450a9f1248
ajin <aj5210@naver.com>
2024-10-19 17:21:52 +0900

    Feat: 친구 섹션을 로그인 여부로 동적 렌더링 추가

--- src/pages/Layout/MainLayout/MemberSection/index.tsx
+++ src/pages/Layout/MainLayout/MemberSection/index.tsx
@@ -1,4 +1,7 @@
+import { Center, Text } from '@chakra-ui/react'
+
 import { PageLayout } from '@/components/PageLayout'
+import { useAuthTokenStore } from '@/stores/auth-token'
 import { useMemberTypeStore } from '@/stores/member-type'
 
 import { FriendHeader } from './FriendHeader'
@@ -8,6 +11,16 @@ import { SelectFriendSection } from './SelectFriendSection'
 
 export const MemberSection = () => {
   const memberType = useMemberTypeStore((state) => state.memberType)
+  const isLoggedIn = useAuthTokenStore((state) => state.isLoggedIn())
+
+  if (!isLoggedIn)
+    return (
+      <PageLayout.SideSection SectionHeader={<FriendHeader />}>
+        <Center paddingTop={2}>
+          <Text fontSize="small">로그인 후 이용해주세요.</Text>
+        </Center>
+      </PageLayout.SideSection>
+    )
 
   return (
     <>
