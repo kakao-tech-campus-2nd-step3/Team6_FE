df02aeb0237971831e3646750fbd1d577a547011
ajin <aj5210@naver.com>
2024-10-21 16:52:06 +0900

    Feat(member): 친구 설정 완료 시 성공 팝업 추가

--- src/pages/Layout/MainLayout/MemberSection/SelectFriendHeader/index.tsx
+++ src/pages/Layout/MainLayout/MemberSection/SelectFriendHeader/index.tsx
@@ -1,9 +1,10 @@
-import { BiUserCheck } from 'react-icons/bi'
+import { BiCheckCircle, BiUserCheck } from 'react-icons/bi'
 
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text, useDisclosure } from '@chakra-ui/react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { addFriends } from '@/api/services/friend'
+import { AlertModal } from '@/components/Modal/AlertModal'
 import { PageLayout } from '@/components/PageLayout'
 import { useFriendStore } from '@/stores/friends'
 import { useMemberTypeStore } from '@/stores/member-type'
@@ -11,6 +12,7 @@ import { Friend } from '@/types'
 
 export const SelectFreindHeader = () => {
   const queryClient = useQueryClient()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const setMemberType = useMemberTypeStore((state) => state.setMemberType)
 
@@ -20,26 +22,38 @@ export const SelectFreindHeader = () => {
   const { mutate: onClickAddFriends } = useMutation({
     mutationFn: () => addFriends({ friends: friendsId }),
     onSuccess: () => {
-      setMemberType('FRIEND')
       queryClient.invalidateQueries({ queryKey: ['friends'] })
+      onOpen()
     },
   })
 
   return (
-    <PageLayout.SideSection.SectionHeader
-      Icon={BiUserCheck}
-      title="친구 설정"
-      Extentions={
-        <Box
-          _hover={{ color: 'black.800', cursor: 'pointer' }}
-          onClick={() => onClickAddFriends()}
-        >
-          <Text fontSize="small" fontWeight="bold">
-            설정 완료
-          </Text>
-        </Box>
-      }
-    />
+    <div>
+      <PageLayout.SideSection.SectionHeader
+        Icon={BiUserCheck}
+        title="친구 설정"
+        Extentions={
+          <Box
+            _hover={{ color: 'black.800', cursor: 'pointer' }}
+            onClick={() => onClickAddFriends()}
+          >
+            <Text fontSize="small" fontWeight="bold">
+              설정 완료
+            </Text>
+          </Box>
+        }
+      />
+      <AlertModal
+        isOpen={isOpen}
+        onClose={() => {
+          onClose()
+          setMemberType('FRIEND')
+        }}
+        icon={<BiCheckCircle />}
+        title="친구 설정이 완료되었습니다."
+        description=""
+      />
+    </div>
   )
 }
 
